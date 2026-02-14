2cc891194b2b55f8025e05c5ebb3654511dcac3d
satyamjha003 <satyamkumarspj2006@gmail.com>
2026-02-14 18:31:39 +0530

    Configured frontend to send request on backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://blogger-v4p3.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
